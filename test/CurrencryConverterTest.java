823ca9a435de2c33cc55c255f2f94be67bbbf80b
Dolby <olipod89@gmail.com>
2023-11-29 14:01:25 -0500

    modification pour run junit sur vscode sans maven

---
+++ test/CurrencryConverterTest.java
@@ -0,0 +1,11 @@
+package test;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class CurrencryConverterTest {
+    
+    @Test
+    public void test(){
+        System.out.println("Hey");
+    }
+}
