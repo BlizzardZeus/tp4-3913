fb433d660a4a3411e8d00ed988027ee6c38009c7
Dolby <olipod89@gmail.com>
2023-12-01 17:32:20 -0500

    quelques tests update

--- test/CurrencryConverterTest.java
+++ test/CurrencryConverterTest.java
@@ -1,11 +1,74 @@
 package test;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.*;
 import org.junit.Assert.*;
+import currencyConverter.Currency;
+import currencyConverter.CurrencyConverter;
+import currencyConverter.MainWindow;
 
 public class CurrencryConverterTest {
-    
-    @Test
-    public void test(){
-        System.out.println("Heyy");
+    private ArrayList<Currency> currencies;
+
+    @Before
+    public void setUp() {
+
+        currencies = Currency.init();
+        
+
     }
+
+    //Test de Boite Noire
+    @Test
+    public void testNegativeAmountConversion() {
+        double negativeAmount = -100.0;
+
+        double conversion = MainWindow.convert("USD", "EUR", currencies, negativeAmount);
+        assertEquals(0, conversion, 0.001);
+    }
+
+    @Test
+    public void testZeroAmountConversion() {
+        double zeroAmount = 0.0;
+        double expectedResult = 0.0;
+        
+        double actualResult = MainWindow.convert("USD", "EUR", currencies, zeroAmount);
+    
+        assertEquals(expectedResult, actualResult, 0.001);
+    }
+
+    @Test
+    public void testExtremeAmountConversion(){
+        double amount = 1000000.0;
+        double expectedResult = 930000.0;
+
+        double actualResult = MainWindow.convert("US Dollar", "Euro", currencies, amount);
+
+        assertEquals(expectedResult, actualResult, 0.001);
+    }
+
+    @Test
+    public void testUnAcceptedAmoutConversion(){
+        double unacceptedAmout = 10000000.0;
+
+        double actualResult = MainWindow.convert("USD", "EUR", currencies, unacceptedAmout);
+    
+        assertEquals(0, actualResult, 0.001);
+    }
+
+
+    //Test de Boite Blanche
+    @Test
+    public void test
+
+
+
+    
 }
